90f037a96ee5ad82f1f567efdd5c44c0eadceb8d
aaravss <104297694+aaravss@users.noreply.github.com>
2024-03-10 18:30:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 status = "";
 object = "";
+objects = [];
+
+
 function setup(){
     canvas = createCanvas(500,500);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(500,500);
     video.hide();
+    alarm = loadSound("security-alarm-80493.mp3");
+    alarm.setVolume(0.5);
 }
 
 function start(){
     objectDetector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById('status').innerHTML = 'Status: detecting objects..';
+    document.getElementById('statusm').innerHTML = 'Status: detecting objects...';
     object = document.getElementById('input').value;
 }
 
@@ -20,7 +25,43 @@ status = true;
 }
 
 function draw(){
-    translate(width,0); 
-  scale(-1.0,1.0);   
-    image(video,0,0,500,500);
+      image(video,0,0,500,500);
+
+    if(status != ""){
+        objectDetector.detect(canvas,gotResult);
+        for(i = 0; i<objects.length; i++){
+            percent = floor(objects[i].confidence*100);
+            label = objects[i].label;
+            stroke("#FF0000");
+            fill("#FF0000");
+            text(label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(object == objects[i].label){
+                objectDetector.detect(gotResult);
+                video.stop()
+                document.getElementById("statuso").innerHTML = "Object found!"
+                var synth = window.speechSynthesis;
+
+                speak_data = objects[i].label + " found!";
+            
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+            
+                synth.speak(utterThis);
+
+                alarm.play();
+                
+          }
+        }
+    }
 }
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
